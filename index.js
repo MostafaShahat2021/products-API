ba01e804dcb001f8ecb0ef94e82df8cfa19b38d8
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2024-08-17 23:40:47 +0300

    Add vercel.json

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require('mongoose');
 const Product = require("./models/product.model");
 const productRoute = require("./routes/product.route");
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 
